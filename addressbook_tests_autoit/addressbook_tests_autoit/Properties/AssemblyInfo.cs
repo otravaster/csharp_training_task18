c9a3a5362766a3c8f232c4c97e6a044c36aad54f
COINS\Maria.Zavgorodneva <Maria.Zavgorodneva@coins-global.com>
2021-08-31 18:37:12 +0300

    Task18-Problem-with-test-run

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("930e21f0-0fec-4690-a67b-b8f89d75e352")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupDeletionTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupDeletionTests : TestBase
+    {
+        [Test]
+        public void TestGroupDeletion()
+        {
+            GroupData newGroup = new GroupData()
+            {
+                Name = "mztest"
+            };
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            if (oldGroups == null)
+            {
+                app.Groups.Add(newGroup);
+                oldGroups = app.Groups.GetGroupList();
+            }
+
+            GroupData toBeDeleted = oldGroups[0];
+            
+            app.Groups.Delete(toBeDeleted);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Remove(toBeDeleted);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
